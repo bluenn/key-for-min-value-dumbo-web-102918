98c621c6406bb0623fb4c4fdc40ee708a787733e
a lan <bluenowte97@gmail.com>
2018-11-08 19:12:46 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+if name_hash = {}
+  nil
 
+min = 100
+small = ""
+
+name_hash.do |furniture, value|
+  if value < min
+    value = min
+    small = furniture
+  end
+ end
+ furniture
 end
